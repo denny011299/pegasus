7ba61eec9786037b8e03f75a1f6a1bfb47b04fb4
Eric-Jonathan <ericjonathanliem@gmail.com>
2026-02-04 17:51:03 +0700

    Pengiriman

--- public/Custom_js/Backoffice/Customers/Sales_Order.js
+++ public/Custom_js/Backoffice/Customers/Sales_Order.js
@@ -419,7 +419,7 @@
     //delete
     $(document).on("click",".btn_delete",function(){
         var data = $('#tableSalesOrder').DataTable().row($(this).parents('tr')).data();//ambil data dari table
-        showModalDelete("Apakah yakin ingin menghapus pesanan penjualan ini?","btn-delete-sales");
+        showModalDelete("Apakah yakin ingin menghapus pengiriman ini?","btn-delete-sales");
         $('#btn-delete-sales').attr("so_id", data.so_id);
     });
 
@@ -435,7 +435,7 @@
             success:function(e){
                 $('.modal').modal("hide");
                 refreshSalesOrder();
-                notifikasi('success', "Berhasil Delete", "Berhasil delete pesanan penjualan");
+                notifikasi('success', "Berhasil Delete", "Berhasil delete pengiriman");
                 
             },
             error:function(e){
