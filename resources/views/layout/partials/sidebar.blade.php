1ff46a2f256bb098240975319a9fb7f650a00553
rubenyw <104652395+rubenyw@users.noreply.github.com>
2026-08-14 16:49:07 +0700

    Update sidebar.blade.php

--- resources/views/layout/partials/sidebar.blade.php
+++ resources/views/layout/partials/sidebar.blade.php
@@ -822,6 +822,11 @@
             if (! $akses->firstWhere('name', $name)) {
                 return false;
             }
+            // Master gudang harus tetap bisa dibuka dari gudang mana pun
+            // (kalau ikut whitelist, menu bisa hilang dan tidak bisa diedit lagi).
+            if (in_array($name, ['Gudang', 'Tipe Gudang'], true)) {
+                return true;
+            }
             if (! $activeWh) {
                 return true;
             }
